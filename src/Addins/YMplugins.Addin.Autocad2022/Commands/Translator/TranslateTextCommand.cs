b329f816e2562de98fe91197b6fde9d2626a098a
yusufzhon.marasulov <yusufzhon.marasulov@YU-MARASULOV-UZ>
2024-09-05 15:07:55 +0500

    before editing old csproj

---
+++ src/Addins/YMplugins.Addin.Autocad2022/Commands/Translator/TranslateTextCommand.cs
@@ -0,0 +1,41 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using SimpleInjector;
+using YMplugins.Models.Acad2022.Services;
+using YMplugins.Services.SettingsReader;
+using YMplugins.Services.Translator;
+using YMplugins.Services.Translator.Google;
+
+namespace YMplugins.Models.Acad2022.Commands.Translator
+{
+
+    public class TranslateTextCommand
+    {
+        public void TranslateText(TranslationSettings settings)
+        {
+            //var settings = new TranslationSettings
+            //{
+            //    SourceLanguage = _selectedSourceLanguage ?? "auto", // Значение из sourceCombo
+            //    TargetLanguage = _selectedTargetLanguage ?? "en"    // Значение из targetCombo
+            //};
+
+            // Контейнер и регистрация зависимостей
+            var container = new Container();
+            container.RegisterInstance(settings);
+            container.Register(() => new SettingsService());
+            container.Register<ITextTranslator, GoogleTranslateService>();
+            container.Register(() =>
+            {
+                var settingsService = container.GetInstance<SettingsService>();
+                var config = settingsService.LoadSettings();
+                return new DictionaryService(config.DictFilePath);
+            }, Lifestyle.Singleton);
+
+            container.Register<TextProcessor>();
+
+            container.Verify();
+            var textProcessor = container.GetInstance<TextProcessor>();
+
+            textProcessor.ProcessTexts(settings);
+        }
+    }
+}
---
+++ src/Addins/YMplugins.Addin.Autocad2022/Commands/Translator/TranslateWithOriginalCommand.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using SimpleInjector;
+using YMplugins.Addin.Acad2022.Commands.Translator;
+using YMplugins.Models.Acad2022.Services;
+using YMplugins.Services.SettingsReader;
+using YMplugins.Services.Translator;
+using YMplugins.Services.Translator.Google;
+
+namespace YMplugins.Models.Acad2022.Commands.Translator
+{
+    public class TranslateWithOriginalCommand
+    {
+        [CommandMethod("YmTranslate")]
+        public void TranslateText()
+        {
+            var settings = new TranslationSettings
+            {
+                SourceLanguage = TranslationState.Instance.SelectedSourceLanguage,
+                TargetLanguage = TranslationState.Instance.SelectedTargetLanguage
+            };
+
+            var container = new Container();
+            container.RegisterInstance(settings);
+            container.Register(() => new SettingsService());
+            container.Register<ITextTranslator, GoogleTranslateService>();
+            container.Register(() =>
+            {
+                var settingsService = container.GetInstance<SettingsService>();
+                var config = settingsService.LoadSettings();
+                return new DictionaryService(config.DictFilePath);
+            }, Lifestyle.Singleton);
+
+            container.Register<TextProcessor>();
+
+            container.Verify();
+            var textProcessor = container.GetInstance<TextProcessor>();
+
+            textProcessor.ProcessTexts(settings);
+
+        }
+    }
+}
