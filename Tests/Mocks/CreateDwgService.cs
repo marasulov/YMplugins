92efaa158b56c286a33cd3377dff0d2fcd27af65
yusufzhon.marasulov <yusufzhon.marasulov@YU-MARASULOV-UZ>
2024-11-08 16:09:23 +0500

    5464

---
+++ Tests/Mocks/CreateDwgService.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using YMplugins.Contracts;
+using YMplugins.Contracts.Dto;
+
+namespace Mocks
+{
+    public class CreateDwgService : ICreateDwgService
+    {
+        public string[] Create(PrintInfo[] printData)
+        {
+            return "throw new NotImplementedException();".ToArray().Select(x => x.ToString()).ToArray();
+        }
+    }
+}
